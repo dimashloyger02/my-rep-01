5f8d617f90988be3cea985f7d734f614311ddba1
dimashloyger02 <94590260+dimashloyger02@users.noreply.github.com>
2025-12-25 22:45:24 +0700

    Update init.js

--- clicker/assets/js/init.js
+++ clicker/assets/js/init.js
@@ -1,1 +1,5 @@
-
+// В init.js или другом месте
+console.log('Файлы подключены:', 
+  !!document.querySelector('link[href="css/style.css"]'),
+  !!document.querySelector('script[src="js/core.js"]')
+);
